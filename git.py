2eb277c6a50d8edf3f4c7e5b5fb4bf69a1fb01fa
Silver <edgar.valmana@icloud.com>
2026-03-28 21:20:05 -0700

    first commit

---
+++ git.py
@@ -0,0 +1,37 @@
+#git
+#its a tool inside your folder and takes snapshots of your code over time, they're called commits
+#you decide when you take a snapshot and you write a short message explaining what changed
+#as a result you get an entire history of your project that you can go back and see at any snapshot you took
+
+
+#GIT IS NOT THE SAME AS GITHUB
+#git is the camera and github is the website where you upload your Git history, so its stored online, backed up and shareable.
+
+"""
+basic git workflow
+1. make changes to your file
+2. git add = putting items in the box
+3. git commit = sealing box and labelling it
+4. git push = sending the box to Github
+"""
+
+"""
+git init = Creates a new Git tracker inside a folder #so that git sees this folder, its like importing git except this is not a library
+git status = Shows what has changed since the last commit
+git add . = Stages all changed files (puts everything in the box)
+git commit -m "message" = Takes the snapshot with a label
+git push = Uploads to GitHub
+git log = Shows the history of all commits
+git clone = Downloads a repo from GitHub to your computer
+"""
+
+
+#initialize git= git init (in terminal) #this starts the folder tracking for git to see your whole folder
+
+#make a file called README.md (written in markdown) 
+
+#git status to see the status of your files in the terminal. #this is to see the previous commits you have made
+
+#git add . (. to add everything in the folder) #puts in box
+
+#git commit -m "message"
